d9d7754bcf6f07cffd2a9f4fb7facf750cb5db96
gcaraman <gcaraman@mitocgroup.com>
2018-03-28 14:28:11 +0300

    modified the name of the bucket match the existing one

--- iam/policies/power-user-policy/config.tf
+++ iam/policies/power-user-policy/config.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = "0.11.0"
 
   backend "s3" {
-    bucket = "cloudnativeci-metadata"
+    bucket = "cnci-metadata"
     key    = "cache/MitocGroup/terraform-demo/iam/policies/power-user-policy/terraform.tfstate"
     region = "us-east-1"
   }
--- iam/roles/admin-role/config.tf
+++ iam/roles/admin-role/config.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = "0.11.0"
 
   backend "s3" {
-    bucket = "cloudnativeci-metadata"
+    bucket = "cnci-metadata"
     key    = "cache/MitocGroup/terraform-demo/iam/roles/admin-role/terraform.tfstate"
     region = "us-east-1"
   }
